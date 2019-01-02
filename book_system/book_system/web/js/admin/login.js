9af0bbf6d023eb9b8a12d4b0e3358edc8a66a9cf
qywuyanzu <1806804351@qq.com>
2019-01-02 19:54:56 +0800

    add demo

--- book_system/book_system/web/js/admin/login.js
+++
@@ -1,7 +0,0 @@
-(function(){
-
-    $(document).ready(function(){
-        bookSystem.login('../../php/admin/login.php','./index.php');
-    })
-
-}());
--- book_system/book_system/web/js/user/login.js
+++
@@ -1,7 +0,0 @@
-(function(){
-
-    $(document).ready(function(){
-        bookSystem.login('../../php/user/login.php','./index.php');
-    })
-
-}());
